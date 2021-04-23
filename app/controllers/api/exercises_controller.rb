610ffd402ab6827da582d1237fb2e1df22e0618b
Eric <lee.erictae@gmail.com>
2021-04-22 19:24:23 -0500

    migration to exercise, add index action to exercise

--- app/controllers/api/exercises_controller.rb
+++ app/controllers/api/exercises_controller.rb
@@ -1,8 +1,18 @@
 class Api::ExercisesController < ApplicationController
   def index
-    @exercises = Exercise.all
-    render "index.json.jb"
+
+    response = HTTP.get("https://wger.de/api/v2/exercise/?format=json")
+    exercises = response.parse
+    p response.parse
+    
+    # @exercises.each do |exercise|
+    #   p exercise["results"]
   end
+
+
+    # @exercises = Exercise.all
+    # render "index.json.jb"
+ 
 
   def show
     @exercise = Exercise.find_by(id: params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_003102) do
+ActiveRecord::Schema.define(version: 2021_04_20_060336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_04_09_003102) do
     t.integer "equipment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "muscle_primary"
+    t.integer "muscle_secondary"
   end
 
   create_table "muscle_exercises", force: :cascade do |t|
