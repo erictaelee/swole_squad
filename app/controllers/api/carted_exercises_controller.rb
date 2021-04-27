01396e6609fc1b40c0e5cceed59085b7fc8b8151
Eric <lee.erictae@gmail.com>
2021-04-26 21:04:44 -0500

    add create action for CartedExerise, make it dynamic, set associations

--- app/controllers/api/carted_exercises_controller.rb
+++ app/controllers/api/carted_exercises_controller.rb
@@ -1,5 +1,14 @@
 class Api::CartedExercisesController < ApplicationController
   def create
+
+    @carted_exercise = CartedExercise.new(
+      user_id: current_user.id,
+      muscle_id: params[:muscle_id],
+      exercise_id: params[:exercise_id],
+      status: "carted",
+      
+    )
+    @carted_exercise.save!
     render 'show.json.jb'
   end
 end
