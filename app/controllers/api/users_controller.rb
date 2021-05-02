d2720c6d9a83efd7c7444e6af8db209ddbcaa69b
Eric <lee.erictae@gmail.com>
2021-05-02 12:04:41 -0500

    add user index and show

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,30 @@
 class Api::UsersController < ApplicationController
+  def index
+    # render json: "message: People are signing up!"
+    user = current_user
+    p user
+    admin = user.adminid
+    p admin
+    if current_user && admin == "a0001"
+      @users = User.all
+      render "index.json.jb"
+    else
+      p current_user
+      @users = []
+      render json: { message: "Access Denied (UIndex)" }
+    end
+  end
+
+  def show
+    if current_user
+      @user = current_user
+      render "show.json.jb"
+    else
+      @user = []
+      render json: { message: "Access Denied (UShow)" }
+    end
+  end
+  
   def create
     user = User.new(
       name: params[:name],
