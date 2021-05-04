1e80bf307833a33535359fb9dab2b648e01d8bf7
Eric <lee.erictae@gmail.com>
2021-05-03 20:09:02 -0500

    add name to login

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -10,7 +10,7 @@ class Api::SessionsController < ApplicationController
         Rails.application.secrets.fetch(:secret_key_base), # the secret key
         "HS256" # the encryption algorithm
       )
-      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
+      render json: { jwt: jwt, email: user.email, user_id: user.id, name: user.name }, status: :created
     else
       render json: {}, status: :unauthorized
     end
